56eb256553a0314efcfebebdefd8e13bdb8fa5c1
Mark Thomes <mthomes@letspersuade.com>
2023-04-25 20:21:24 -0500

    first commit

---
+++ src/icons/IconArrow.svelte
@@ -0,0 +1,14 @@
+<svg
+  fill="none"
+  height="20"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  stroke-width="2"
+  stroke="currentColor"
+  viewBox="0 0 24 24"
+  width="20"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <line x1="5" y1="12" x2="19" y2="12"/>
+  <polyline points="12 5 19 12 12 19"/>
+</svg>
